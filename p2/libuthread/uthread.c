ec96188fee9fab7956bbb32b24225967ed2b8336
Seungeui Song <69485883+SeungeuiSong@users.noreply.github.com>
2023-05-10 18:18:18 -0700

    Update uthread.c

--- p2/libuthread/uthread.c
+++ p2/libuthread/uthread.c
@@ -1,70 +1,77 @@
-#include <assert.h>
-#include <signal.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/time.h>
-
-#include "private.h"
-#include "uthread.h"
-#include "queue.h"
-
-
-struct uthread_tcb *process;
-queue_t ready_list;
-queue_t waiting_list;
-
-struct uthread_tcb {
-	/* TODO Phase 2 */
-    ucontext_t context;
-    void *stack; 
-    int state; 
-    int tid;
-};
-
-struct uthread_tcb *uthread_current(void)
+void uthread_exit(void)
 {
-	/* TODO Phase 2/3 */
-	getcontext(process->context);
-	return process;
-}
-
-void uthread_yield(void)
-{
-	preempt_disable();
+    preempt_disable();
     struct uthread_tcb *prev_thread = process;
-    queue_enqueue(&ready_list, prev_thread);
+    prev_thread->state = 0;
+    free(prev_thread->stack);
+    queue_delete(&ready_list, prev_thread);
     struct uthread_tcb *next_thread = queue_dequeue(&ready_list);
     process = next_thread;
     uthread_ctx_switch(prev_thread->context, next_thread->context);
     preempt_enable();
-
-	/* TODO Phase 2 */
-}
-
-void uthread_exit(void)
-{
-	/* TODO Phase 2 */
 }
 
 int uthread_create(uthread_func_t func, void *arg)
 {
-	/* TODO Phase 2 */
+    preempt_disable();
+    struct uthread_tcb *new_thread = (struct uthread_tcb *)malloc(sizeof(struct uthread_tcb));
+    if (new_thread == NULL) {
+        preempt_enable();
+        return -1;
+    }
+
+    new_thread->stack = uthread_ctx_alloc_stack();
+    if (new_thread->stack == NULL) {
+        free(new_thread);
+        preempt_enable();
+        return -1;
+    }
+
+    new_thread->tid = queue_length(&ready_list) + 1;
+    new_thread->state = 1;
+
+    if (uthread_ctx_init(&(new_thread->context), new_thread->stack, func, arg) == -1) {
+        free(new_thread->stack);
+        free(new_thread);
+        preempt_enable();
+        return -1;
+    }
+
+    queue_enqueue(&ready_list, new_thread);
+    preempt_enable();
+    return new_thread->tid;
 }
 
 int uthread_run(bool preempt, uthread_func_t func, void *arg)
 {
-	/* TODO Phase 2 */
+    if (preempt) {
+        preempt_start();
+    }
+    int tid = uthread_create(func, arg);
+    if (tid == -1) {
+        return -1;
+    }
+    uthread_yield();
+    return tid;
 }
 
 void uthread_block(void)
 {
-	/* TODO Phase 3 */
+    preempt_disable();
+    struct uthread_tcb *prev_thread = process;
+    prev_thread->state = 0;
+    queue_enqueue(&waiting_list, prev_thread);
+    struct uthread_tcb *next_thread = queue_dequeue(&ready_list);
+    process = next_thread;
+    uthread_ctx_switch(prev_thread->context, next_thread->context);
+    preempt_enable();
 }
 
 void uthread_unblock(struct uthread_tcb *uthread)
 {
-	/* TODO Phase 3 */
+    preempt_disable();
+    uthread->state = 1;
+    queue_delete(&waiting_list, uthread);
+    queue_enqueue(&ready_list, uthread);
+    preempt_enable();
 }
-
